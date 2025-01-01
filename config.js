5f0f6cb64bd109eef7a905fb9f5100fb620e8858
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-01-02 02:01:32 +0600

    config updated

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 const RAMADAN_START_DATE = "2021-04-13";
 const RAMADAN_END_DATE = "2021-05-12";
 
+const DEFAULT_DISTRICT = "Dhaka";
+
 module.exports = {
   RAMADAN_START_DATE,
   RAMADAN_END_DATE,
