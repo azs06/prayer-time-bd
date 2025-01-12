6a113f83a1ffa103b31f5e66050f4ff829b5b23f
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-01-12 17:47:01 +0600

    fixed error

--- data/districts/data.js
+++ data/districts/data.js
@@ -1,7 +1,14 @@
 import { readFileSync } from "fs";
+import path from "node:path";
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const DISTRICS_DATA_FILE = path.join(__dirname, "districts.json");
 
 export const districtData = JSON.parse(
-  readFileSync("./districts.json", "utf8")
+  readFileSync(DISTRICS_DATA_FILE, "utf8")
 );
 
 export function validateDistrict(district) {
